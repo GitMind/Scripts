1b3ff64f59297f5a92704b0547f4d315bcc8760e
gitmind <dnseroshtanov@hotmail.com>
2016-06-07 15:22:23 +0300

    Commit

--- powershell/eventlog_security_done.ps1
+++ powershell/eventlog_security_done.ps1
@@ -6,28 +6,31 @@ $date = $date.ToShortDateString()
 Write-Host "Система: 1-СМА, 2-ИСУР, 3-ЦХЭД, 4 - ИСУР ВП, 5 - МКР"
 $system = Read-Host
 Switch($system){
-1 {$connections = Get-Content D:\Scripts\sma.txt 
+1 {$connections = Get-Content D:\Scripts\EventLog\sma.txt 
    $system = "СМА"
-   $fullUserName = "pasport.local\Administrator"}
-2 {$connections = Get-Content D:\Scripts\isur.txt 
+   $fullUserName = "WORKGROUP\Administrator"}
+2 {$connections = Get-Content D:\Scripts\EventLog\isur.txt 
    $system = "ИСУР"
    $fullUserName = "pasport.local\Administrator"}
-3 {$connections = Get-Content D:\Scripts\ched.txt 
+3 {$connections = Get-Content D:\Scripts\EventLog\ched.txt 
    $system = "ЦХЭД"
    $fullUserName = "pasport.local\Administrator"}
-4 {$connections = Get-Content D:\Scripts\isurvp.txt 
+4 {$connections = Get-Content D:\Scripts\EventLog\isurvp.txt 
    $system = "ИСУР_ВП"
    $fullUserName = "pasport.local\Administrator"}
-5 {$connections = Get-Content D:\Scripts\mkr.txt 
-   $system = "МК"
+5 {$connections = Get-Content D:\Scripts\EventLog\mkr.txt 
+   $system = "МКР"
    $fullUserName = "pasport.local\Administrator"}
 }
 $excel = New-Object -ComObject Excel.Application
 $excel.Visible = $true
 $workbook = $excel.Workbooks.Add()
 foreach($con in $connections){
-    Write-Host "Введите пароль для сервера "$con
-    $log = Invoke-Command -ComputerName $con -ScriptBlock{Get-EventLog -LogName Security | Where-Object {($_.EventID -eq 4624) -and (($_.ReplacementStrings[8] -eq 2) -or ($_.ReplacementStrings[8] -eq 10)) -and ($_.TimeGenerated -ge "02/20/2016")}} -Credential $fullUserName   
+    $comp_dns_name_tmp = $con.ToString().Split(';')
+    $comp_dns_name = $comp_dns_name_tmp[1]
+    $con=$comp_dns_name_tmp[0]
+    Write-Host "Введите пароль для сервера "$comp_dns_name
+    $log = Invoke-Command -ComputerName $con -ScriptBlock{Get-EventLog -LogName Security | Where-Object {($_.EventID -eq 4624) -and (($_.ReplacementStrings[8] -eq 2) -or ($_.ReplacementStrings[8] -eq 10)) -and ($_.TimeGenerated -ge $date)}} -Credential $fullUserName   
     $sheet = $workbook.Worksheets.Add()
     $sheet.Name = $con
     $counter = 0
@@ -41,4 +44,4 @@ foreach($con in $connections){
 $filename = "D:\Security_"+$system+"_"+$date+".xlsx"
 $excel.ActiveWorkbook.SaveAs($filename)
 $excel.Quit()
-Write-Host "Данные сохранены в файл D:\Security_"$system"_"$date".xlsx"
+Write-Host "Данные сохранены в файл C:\Security_"$system"_"$date".xlsx"
